e4a7a4c3ea2d3b1a0214efc3db39e426a0412f60
Ryan Wolski <rwolski@gmail.com>
2016-06-22 19:36:28 +1000

    Add project files.

---
+++ MicroService/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MicroService.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
